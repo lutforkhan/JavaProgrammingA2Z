927c6774bfe026b14e009ee66d8bc70994367b1f
Muhammad Khan <lfk485@yahoo.com>
2018-01-16 22:36:13 -0500

    Book all chapters

---
+++ src/chapter03/J03_03.java
@@ -0,0 +1,13 @@
+package chapter03;
+
+public class J03_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	char Ch1 = 66, Ch2= 'A';
+	System.out.println("Ch1  is " +Ch1);
+	System.out.println("Ch2  is " + Ch2);
+	
+	}
+
+}
